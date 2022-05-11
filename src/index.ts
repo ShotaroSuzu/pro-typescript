ce69b8f085b399a093f9317b45b0c3bc458fa8bd
ShotaroSuzu <cosmos.1220.3110@gmail.com>
2022-05-11 09:30:16 +0900

    2.2.1 case1

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
-const greeting = "Hello, "; //この1行は文。"Hello, "の部分は式
-const text = greeting + "world!";
+const greeting = "Hello, ", //カンマ区切りで宣言はできるが、あまり好まれない。(変数の増減時にやりづらい)
+  text = greeting + "world!";
 
 console.log(text);
+
+const あいう = 123; // 日本語も使える. 変数は数字先頭はNG. Typescript界隈では、変数名は小文字始まりで、型名は大文字始まりが多い
+const 鈴木 = あいう + 876;
+console.log(鈴木);
