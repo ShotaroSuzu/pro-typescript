d24b6faf3e07062c2f5bef9a07f1e7bafa9a363a
ShotaroSuzu <cosmos.1220.3110@gmail.com>
2022-05-15 14:57:43 +0900

    3.1.4 object properties

--- src/section3/objectPractise.ts
+++ src/section3/objectPractise.ts
@@ -1,4 +1,6 @@
-const kick = () => {
+import { createInterface } from "readline";
+
+const objectLiteralPractice1 = () => {
   const obj = {
     foo: 123,
     bar: "Hello, world!",
@@ -10,21 +12,41 @@ const kick = () => {
 
   const user = { name, age: 20 }; // 省略記法
   console.log(user);
+};
+//objectLiteralPractice1();
 
+const objectLiteralPractice2 = () => {
   const propName = "calculated PropName";
 
-  const stringLiteral = {
+  const objectLiteral = {
     foo: "normal foo",
     "foo bar": "string literal foo bar",
-    1: "number literal 1",
+    1: "number literal 1", // "1": "number literal 1" と同じ意味
     propName: `calculated prop name is ${propName}`,
   };
-  console.log(stringLiteral);
-  console.log(stringLiteral.foo);
+  console.log(objectLiteral);
+  console.log(objectLiteral.foo);
   //   console.log(objectLiteral.foo bar)当然こういった書き方はできない
-  console.log(stringLiteral["foo bar"]);
+  console.log(objectLiteral["foo bar"]);
   //   console.log(stringLiteral.1) こういった書き方もできない
-  console.log(stringLiteral[1]);
-  console.log(stringLiteral.propName);
+  console.log(objectLiteral[1]);
+  console.log(objectLiteral.propName);
+
+  objectLiteral.foo = "modified foo";
+  console.log(objectLiteral);
+
+  const rl = createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+  const messages = {
+    good: "0以上の数値が入力されました",
+    bad: "負の数値を入力しないでください",
+  };
+  rl.question("数値を入力してください:", (line) => {
+    const num = Number(line);
+    console.log(messages[num >= 0 ? "good" : "bad"]); // アクセスするプロパティを動的に決める場合は `obj['property name']` という形になる
+    rl.close();
+  });
 };
-kick();
+//objectLiteralPractice2();
