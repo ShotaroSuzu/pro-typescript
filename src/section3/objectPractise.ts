1c942a61d0cd3db6649b13d98707c11a47290a2a
ShotaroSuzu <cosmos.1220.3110@gmail.com>
2022-05-15 14:42:32 +0900

    3.1.3 object properties

--- src/section3/objectPractise.ts
+++ src/section3/objectPractise.ts
@@ -1,6 +1,30 @@
-const obj = {
-  foo: 123,
-  bar: "Hello, world!",
+const kick = () => {
+  const obj = {
+    foo: 123,
+    bar: "Hello, world!",
+  };
+  console.log(obj.foo);
+  console.log(obj.bar);
+
+  const name = "suzuki";
+
+  const user = { name, age: 20 }; // 省略記法
+  console.log(user);
+
+  const propName = "calculated PropName";
+
+  const stringLiteral = {
+    foo: "normal foo",
+    "foo bar": "string literal foo bar",
+    1: "number literal 1",
+    propName: `calculated prop name is ${propName}`,
+  };
+  console.log(stringLiteral);
+  console.log(stringLiteral.foo);
+  //   console.log(objectLiteral.foo bar)当然こういった書き方はできない
+  console.log(stringLiteral["foo bar"]);
+  //   console.log(stringLiteral.1) こういった書き方もできない
+  console.log(stringLiteral[1]);
+  console.log(stringLiteral.propName);
 };
-console.log(obj.foo);
-console.log(obj.bar);
+kick();
