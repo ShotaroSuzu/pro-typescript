5361a40b92c4869d1b61765491dc5ccb8aa1d2ae
ShotaroSuzu <cosmos.1220.3110@gmail.com>
2022-05-29 15:32:33 +0900

    3.7.5 skill test

---
+++ src/section3/skillTest.ts
@@ -0,0 +1,33 @@
+type User = {
+  name: string;
+  age: number;
+  premiumUser: boolean;
+};
+
+const data: string = `
+uhyo,26,1
+Jhom Smith, 17, 0
+Mary Sue,14,1
+`;
+
+const users: User[] = data
+  .split("\n")
+  .filter((line) => line !== "")
+  .map((line) => {
+    const [name, ageString, premiumUserString] = line.split(",");
+    return {
+      name,
+      age: Number(ageString),
+      premiumUser: Boolean(premiumUserString),
+    };
+  });
+
+for (const user of users) {
+  if (user.premiumUser) {
+    console.log(`${user.name} (${user.age}) はプレミアムユーザーです。`);
+  } else {
+    console.log(
+      `${user.name} (${user.age}) はプレミアムユーザーではありません。`
+    );
+  }
+}
